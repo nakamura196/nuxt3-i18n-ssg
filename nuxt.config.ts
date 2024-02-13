68e1254d60c0a965c4d952ba0d7028de09dcc5b9
Satoru Nakamura <na.kamura.1263@gmail.com>
2024-02-14 08:31:27 +0900

    chore: minor update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,6 @@ export default defineNuxtConfig({
   typescript: {
     shim: false, // shimsファイル生成の無効化（VSCodeでVolarを使う場合はfalseにする）
     strict: true, // 型チェックを厳格化
-    typeCheck: false, // nuxt devまたはnuxt build時に型チェックを実行
+    typeCheck: true, // nuxt devまたはnuxt build時に型チェックを実行
   },
 });
